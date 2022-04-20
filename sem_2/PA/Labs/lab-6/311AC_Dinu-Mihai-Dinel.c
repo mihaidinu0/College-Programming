b38882ae01bae2bf1544efb2df9ef4cbc97053c9
dinu.mihai.dinel@gmail.com <dinu.mihai.dinel@gmail.com>
2022-04-20 16:38:32 +0300

    Added to lab 7

--- sem_2/PA/Labs/lab-6/311AC_Dinu-Mihai-Dinel.c
+++ sem_2/PA/Labs/lab-6/311AC_Dinu-Mihai-Dinel.c
@@ -1,3 +1,19 @@
+/*
+Dinu Mihai-Dinel
+311AC
+Lab 7
+*/
+
+/*
+Creați un graf cu minim 10 noduri și 10 muchii. Fiecare muchie va avea un cost asociat.
+
+Realizați arborele minim de acoperire folosind algoritmul lui Kruskal. Afișați ordinea nodurilor, valoarea muchiilor din arbore și costul total. 4.5
+
+Realizați arborele minim de acoperire folosind algoritmul lui Prim. Afișați ordinea nodurilor, valoarea muchiilor din arbore și costul total. 4.5
+
+Tema trebuie să includă și schema grafului.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,19 +34,42 @@ struct Edge{
 void main(){
     
     struct Edge edges ={
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
+        {3, 4, 12},
+        {0, 14, 5},
+        {0, 10, 20},
+        {14, 6, 8},
+        {10, 6, 1},
+        {1, 10, 6},
+        {8, 1, 1},
+        {13, 8, 12},
+        {7, 8, 1},
+        {7, 4, 3},
+        {5, 2, 5},
+        {12, 5, 2},
         {},
     }
 
 }
+
+/*
+3 14 12
+0 14 5
+0 10 20
+14 6 8
+10 6 1
+1 10 6
+8 1 1
+13 8 12
+7 8 1
+7 4 3
+5 2 5
+12 5 2
+15 9 6
+2 15 0
+2 11 1
+5 3 7
+12 10 5
+7 12
+7 12 0
+5 0 200
+*/
