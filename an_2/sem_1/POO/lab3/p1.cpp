f231901de08c37e031115d9269b004e779e39ab8
Mihai Dinu <dinu.mihai.dinel@gmail.com>
2022-10-25 09:29:21 +0300

    Sync

---
+++ an_2/sem_1/POO/lab3/p1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+class Money{
+    private:
+        int amount;
+    public:
+        void set_amount(int a){
+            amount = a;
+        }
+};
+
+int main(){
+
+}
